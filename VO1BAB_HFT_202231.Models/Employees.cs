a11120ad39c17ab7d49a2b94e607bea132507030
VO1BAB <katonabalu@gmail.com>
2022-09-28 14:17:08 +0200

    Employees class created

---
+++ VO1BAB_HFT_202231.Models/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO1BAB_HFT_202231.Models
+{
+    class Employees
+    {
+        public string Name { get; set; }
+
+        public int EmployeesId { get; set; }
+
+        public int Class { get; set; }
+
+        public string PhoneNumer { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public ICollection<Cars> RentCarID { get; set; }
+    }
+}
