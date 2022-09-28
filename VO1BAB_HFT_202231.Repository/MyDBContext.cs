fa01f81862d9038d9f4af5ffabedcd31a0b4dcec
VO1BAB <katonabalu@gmail.com>
2022-09-28 14:50:22 +0200

    Created 3 tables and constructor in mydbcontext

---
+++ VO1BAB_HFT_202231.Repository/MyDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using VO1BAB_HFT_202231.Models;
+
+namespace VO1BAB_HFT_202231.Repository
+{
+    public class MyDBContext:DbContext
+    {
+        public DbSet<Cars> cars { get; set; }
+
+        public DbSet<Employees> employess { get; set; }
+
+        public DbSet<Rents> rents { get; set; }
+
+        public MyDBContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+
+
+
+
+
+
+    }
+}
