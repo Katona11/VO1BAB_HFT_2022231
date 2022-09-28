f7f11f85691e0a6ef8f0c5cbdce152f61474d3da
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-09-28 20:32:49 +0200

    Created the RentsRepository

---
+++ VO1BAB_HFT_202231.Repository/RentsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO1BAB_HFT_202231.Models;
+
+namespace VO1BAB_HFT_202231.Repository
+{
+    class RentsRepository : Repository<Rents>
+    {
+        public RentsRepository(MyDBContext ctx) : base(ctx)
+        {
+        }
+
+        public override Rents Read(int id)
+        {
+            return ctx.rents.FirstOrDefault(t => t.RentId == id);
+        }
+
+        public override void Update(Rents id)
+        {
+            var olditem = Read(id.RentId);
+            foreach (var item in olditem.GetType().GetProperties())
+            {
+                item.SetValue(olditem, item.GetValue(id));
+            }
+        }
+    }
+}
