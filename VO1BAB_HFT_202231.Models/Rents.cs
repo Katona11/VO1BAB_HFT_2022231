27aebfcb44e0222c8498b0f1dac0f2e57f4d4d28
VO1BAB <katonabalu@gmail.com>
2022-09-28 14:25:50 +0200

    Created Rents class

---
+++ VO1BAB_HFT_202231.Models/Rents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO1BAB_HFT_202231.Models
+{
+   public  class Rents
+    {
+        public int RentId { get; set; }
+
+        public string RentTime { get; set; }
+
+        public Cars CarId { get; set; }
+
+        public Employees EmployeesId { get; set; }
+
+
+    }
+}
