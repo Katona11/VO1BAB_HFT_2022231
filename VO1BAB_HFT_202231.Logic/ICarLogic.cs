16e7f325f682581d936d340043aa24fd540b2554
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-09-28 20:54:07 +0200

    Create the CarLogic Interface

---
+++ VO1BAB_HFT_202231.Logic/ICarLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO1BAB_HFT_202231.Models;
+
+namespace VO1BAB_HFT_202231.Logic
+{
+    interface ICarLogic
+    {
+        void Create(Cars item);
+
+        void Delete(int id);
+
+        Cars Read(int id);
+
+        IEnumerable<Cars> ReadAll();
+
+        void Update(Cars item);
+    }
+}
