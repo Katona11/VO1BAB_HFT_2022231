9caedbdbdbd5dd606274cb03f6e762b056011ab9
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-09-28 20:00:18 +0200

    Created the Repository generic interface

---
+++ VO1BAB_HFT_202231.Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO1BAB_HFT_202231.Repository
+{
+    interface IRepository<T> where T:class
+    {
+        T Read(int id);
+
+        IQueryable<T> ReadAll();
+
+        void Create(T item);
+
+        void Delete(int id);
+
+        void Update(int id);
+    }
+}
