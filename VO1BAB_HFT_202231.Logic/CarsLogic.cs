5731fd07ac614b90b921a7f4baef205a718d5b14
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-09-28 21:04:07 +0200

    Create the CarsLogic class

--- VO1BAB_HFT_202231.Logic/CarsLogic.cs
+++ VO1BAB_HFT_202231.Logic/CarsLogic.cs
@@ -1,9 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
+using VO1BAB_HFT_202231.Models;
+using VO1BAB_HFT_202231.Repository;
 
 namespace VO1BAB_HFT_202231.Logic
 {
-    public class CarsLogic
+    public class CarsLogic : ICarLogic
     {
+        IRepository<Cars> repo;
 
+        public CarsLogic(IRepository<Cars> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Cars item)
+        {
+            if (item.Brand.Length<3)
+            {
+                throw new ArgumentException("The BrandName is too short!");
+            }
+            else
+            {
+                this.repo.Create(item);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Cars Read(int id)
+        {
+            var item = this.repo.Read(id);
+            if (item == null)
+            {
+                throw new ArgumentException("The Car not exist!");
+            }
+            else
+            {
+                return item;
+            }
+        }
+
+        public IEnumerable<Cars> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Cars item)
+        {
+            this.repo.Update(item);
+        }
     }
 }
