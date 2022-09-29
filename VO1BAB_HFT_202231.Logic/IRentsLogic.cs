020b2d0a7f2a4d8273ce03a5998c20b6c93f4a2d
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-09-29 22:08:17 +0200

    Create RentsLogic Interface

---
+++ VO1BAB_HFT_202231.Logic/IRentsLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO1BAB_HFT_202231.Models;
+
+namespace VO1BAB_HFT_202231.Logic
+{
+    interface IRentsLogic
+    {
+        void Create(Rents item);
+
+        void Update(Rents item);
+
+        void Delete(int id);
+
+        Rents Read(int id);
+
+        IEnumerable<Rents> ReadAll();
+    }
+}
