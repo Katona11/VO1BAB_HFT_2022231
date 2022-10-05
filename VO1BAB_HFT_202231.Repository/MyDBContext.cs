bc783badfeaa2d0b67ea35467ec52bcdc5682c47
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-10-05 11:47:31 +0200

    Created the half of Employees DataTable #1

--- VO1BAB_HFT_202231.Repository/MyDBContext.cs
+++ VO1BAB_HFT_202231.Repository/MyDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.IO;
 using VO1BAB_HFT_202231.Models;
 
 namespace VO1BAB_HFT_202231.Repository
@@ -93,8 +94,90 @@ namespace VO1BAB_HFT_202231.Repository
 
             modelBuilder.Entity<Employees>().HasData(new Employees[]
             {
+                //100 employees
+                new Employees("Nagy Marcell,1,Management,+367075168691,1961-10-24"),
+                new Employees("Károlyi Lilla,2,Programmers,+367044395260,1957-11-27"),
+                new Employees("Szőke Alexandra,3,Programmers,+367042119345,1952-03-05"),
+                new Employees("Katona Marcell,4,Programmers,+367069748704,1977-09-16"),
+                new Employees("Farkas Dániel,5,Other Workers,+367042791469,1980-07-19"),
+                new Employees("Katona Béla,6,Other Workers,+367031875142,1959-05-03"),
+                new Employees("Medgyesi Mónika,7,Other Workers,+367074667718,1972-03-20"),
+                new Employees("Kovács Bence,8,Administrator,+367031897113,1996-09-26"),
+                new Employees("Farkas Miklós,9,Management,+367003859710,1997-12-04"),
+                new Employees("Cséfalvay Bence,10,Administrator,+367076134780,1976-01-28"),
+                new Employees("Szabó Koppány,11,Programmers,+367069733970,1969-02-07"),
+                new Employees("Kiss Alexandra,12,Programmers,+367028844552,1982-05-29"),
+                new Employees("Medgyesi Dávid,13,Other Workers,+367097668749,1960-10-29"),
+                new Employees("Katona Bence,14,Administrator,+367029142675,1961-05-30"),
+                new Employees("Cséfalvay Koppány,15,Programmers,+367033476815,1976-07-04"),
+                new Employees("Medgyesi Koppány,16,Programmers,+367070558896,1961-06-11"),
+                new Employees("Medgyesi Bálint,17,Programmers,+367052759058,1969-03-07"),
+                new Employees("Lakatos Marcell,18,Administrator,+367046357481,1993-05-16"),
+                new Employees("Kiss Alexandra,19,Other Workers,+367019396702,1996-07-24"),
+                new Employees("Bátory Alex,20,Other Workers,+367073382215,1959-10-28"),
+                new Employees("Bátory Alexandra,21,Programmers,+367043196764,1992-04-11"),
+                new Employees("Katona Marcell,22,Management,+367077806934,2000-08-25"),
+                new Employees("Kovács Marcell,23,Administrator,+367076083584,1973-10-07"),
+                new Employees("Kiss Zsófia,24,Programmers,+367061536883,1999-11-05"),
+                new Employees("Béres Attila,25,Management,+367044141061,1961-07-16"),
+                new Employees("Nagy Alexandra,26,Programmers,+367030867088,1955-05-16"),
+                new Employees("Bátory Alex,27,Programmers,+367071694709,1985-09-27"),
+                new Employees("Varga Dániel,28,Programmers,+367099854508,1954-07-23"),
+                new Employees("Lakatos Bálint,29,Administrator,+367037932340,1995-07-20"),
+                new Employees("Medgyesi Bálint,30,Programmers,+367030754381,1978-12-12"),
+                new Employees("Varga Dávid,31,Other Workers,+367001145459,1990-07-28"),
+                new Employees("Cséfalvay Áron,32,Other Workers,+367013973650,1989-03-16"),
+                new Employees("Katona Bence,33,Management,+367020900696,1971-02-05"),
+                new Employees("Bátory Alex,34,Programmers,+367032623992,1952-08-30"),
+                new Employees("Medgyesi Dávid,35,Management,+367040966377,1980-11-09"),
+                new Employees("Farkas Gergő,36,Programmers,+367097558064,1978-04-16"),
+                new Employees("Bátory Zsófia,37,Management,+367093132823,1960-06-03"),
+                new Employees("Béres Bence,38,Administrator,+367068900523,1954-06-25"),
+                new Employees("Cséfalvay Tamás,39,Other Workers,+367046190772,1990-11-04"),
+                new Employees("Nagy Lilla,40,Administrator,+367050443177,1968-06-24"),
+                new Employees("Kásás Bence,41,Administrator,+367026504385,1967-03-12"),
+                new Employees("Kiss Alex,42,Programmers,+367000993770,2003-05-29"),
+                new Employees("Cséfalvay Lilla,43,Other Workers,+367036139100,1968-01-19"),
+                new Employees("Katona Lőrinc,44,Management,+367072033363,1980-10-13"),
+                new Employees("Lakatos Gergő,45,Management,+367062111116,1952-01-02"),
+                new Employees("Medgyesi Tamás,46,Other Workers,+367068340627,1965-08-30"),
+                new Employees("Bátory Réka,47,Other Workers,+367013902831,2002-05-22"),
+                new Employees("Károlyi Kitti,48,Management,+367020269667,1973-12-29"),
+                new Employees("Varga Áron,49,Other Workers,+367095239147,1955-01-14"),
+                new Employees("Károlyi Marcell,50,Administrator,+367089848152,1967-04-27"),
+//new Employees(Béres Miklós,51,Programmers,+367090008314,1957-4-19),
+//new Employees(Medgyesi Marcell,52,Administrator,+367092230084,1956-6-12),
+//new Employees(Katona Alexandra,53,Administrator,+367019558081,2001-6-22),
+//new Employees(Varga Tamás,54,Programmers,+367085814030,1951-6-13),
+//new Employees(Szabó Mónika,55,Programmers,+367086609521,1990-1-27),
+//new Employees(Tóth Lőrinc,56,Other Workers,+367028786466,1979-3-11),
+//new Employees(Farkas Dávid,57,Management,+367090145323,1992-8-9),
+//new Employees(Szőke Attila,58,Administrator,+367078393571,1990-8-22),
+//new Employees(Kiss Alex,59,Programmers,+367014752126,1998-5-30),
+//new Employees(Szabó Dániel,60,Other Workers,+367000574986,1998-5-4),
+//new Employees(Bátory Dávid,61,Other Workers,+367035855913,2000-10-22),
+//new Employees(Szőke Alexandra,62,Management,+367099099816,1954-12-19),
+//new Employees(Bátory Tamás,63,Programmers,+367053448433,1979-5-24),
+//new Employees(Medgyesi Alex,64,Administrator,+367017055779,1968-2-25),
+//new Employees(Béres Marcell,65,Administrator,+367048975785,1969-3-1),
+//new Employees(Lakatos Alex,66,Administrator,+367046230453,1998-2-7),
+//new Employees(Károlyi Lőrinc,67,Administrator,+367092638696,1973-12-3),
+//new Employees(Kiss Bence,68,Administrator,+367048550157,1998-6-15),
+//new Employees(Kovács Attila,69,Management,+367098590360,1981-8-6),
+//new Employees(Nagy Marcell,70,Administrator,+367012207090,1981-8-19),
+//new Employees(Tóth Attila,71,Programmers,+367031965775,1969-2-29),
+//new Employees(Farkas Gergő,72,Programmers,+367028495756,1965-6-13),
+//new Employees(Lakatos Bálint,73,Other Workers,+367029352374,1986-6-17),
+//new Employees(Szőke Áron,74,Administrator,+367090131586,1960-9-1),
+//new Employees(Cséfalvay Bence,75,Other Workers,+367085255824,1992-5-6),
+//new Employees(Károlyi Dániel,76,Other Workers,+367029367962,1967-2-26),
+//new Employees(Medgyesi Áron,77,Administrator,+367076596951,1959-12-11),
+//new Employees(Nagy Dávid,78,Programmers,+367027159042,1965-5-17),
+//new Employees(Bátory Attila,79,Administrator,+367012799660,1991-3-31),
+//new Employees(Medgyesi Zsófia,80,Management,+367074366195,2002-3-21),
 
-            });
+
+            }); ;
 
            modelBuilder.Entity<Rents>().HasData(new Rents[]
            {
