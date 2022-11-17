fcda036bd183b7b8733faea8d074d76d8e493ef0
katonabalint <katonabalint@stud.uni-obuda.hu>
2022-11-17 19:29:46 +0100

    create first test

--- VO1BAB_HFT_202231.Test/TesterClass.cs
+++ VO1BAB_HFT_202231.Test/TesterClass.cs
@@ -1,8 +1,74 @@
-﻿using System;
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VO1BAB_HFT_202231.Logic;
+using VO1BAB_HFT_202231.Models;
+using VO1BAB_HFT_202231.Repository;
 
 namespace VO1BAB_HFT_202231.Test
 {
+    [TestFixture]
     public class TesterClass
     {
+        CarsLogic logic;
+        CarBrandLogic carbrandlogic;
+        RentsLogic rentlogic;
+        Mock<IRepository<Cars>> mockCarRepo;
+        Mock<IRepository<CarBrand>> carbrandrepo;
+        Mock<IRepository<Rents>> rentsrepo;
+
+
+       [SetUp]
+        public void Init()
+        {
+            mockCarRepo = new Mock<IRepository<Cars>>();
+            mockCarRepo.Setup(m => m.ReadAll()).Returns(new List<Cars>()
+            {
+                new Cars("1,1,E60,HFG-434,2004,310"),
+                new Cars("2,2,AMG,YYY-444,2020,674")
+            }.AsQueryable());
+
+             carbrandrepo = new Mock<IRepository<CarBrand>>();
+             carbrandrepo.Setup(m => m.ReadAll()).Returns(new List<CarBrand>()
+            {
+                new CarBrand("1,Mercedes"),
+                new CarBrand("2,Audi")
+            }.AsQueryable());
+
+            rentsrepo = new Mock<IRepository<Rents>>();
+            rentsrepo.Setup(m => m.ReadAll()).Returns(new List<Rents>()
+            {
+                new Rents("1,2001-06-22,Katona István,1"),
+                new Rents("2,2022-03-11,Kiss Bécy,2")
+            }.AsQueryable());
+
+            logic = new CarsLogic(mockCarRepo.Object);
+            carbrandlogic = new CarBrandLogic(carbrandrepo.Object);
+            rentlogic = new RentsLogic(rentsrepo.Object);
+
+        }
+
+        [Test]
+        public void CreateCarTest()
+        {
+            var car = new Cars()
+            {
+                CarsID = 3,
+                CarBrandID = 2,
+                Type = "Amg",
+                LicensePlateNumber = "HFG-435",
+                Year = 2010,
+                PerformanceInHP = 500
+
+            };
+            logic.Create(car);
+            mockCarRepo.Verify(r => r.Create(car), Times.Once);
+            
+            
+        }
+        
+
     }
 }
