d615df6e46ef37730de5745838620d196124e3d9
katonabalint <katonabalint@stud.uni-obuda.hu>
2023-05-15 22:22:43 +0200

    Update cars entity create and remove method

--- VO1BAB_HFT_202231.JSClient/wwwroot/carsscript.js
+++ VO1BAB_HFT_202231.JSClient/wwwroot/carsscript.js
@@ -1,6 +1,6 @@
-﻿/*let car = [];*/
+﻿let car = [];
 
-/*getData();*/
+getData();
 
 //async function getData() {
 //    await fetch("http://localhost:50437/cars")
@@ -12,68 +12,86 @@
 //        });
 //}
 
-//async function getData() {
-//    let resp = await fetch("http://localhost:50437/cars");
-//    let data = await resp.json();
-//    car = data;
-//    display();
+async function getData() {
+    let resp = await fetch("http://localhost:50437/cars");
+    let data = await resp.json();
+    car = data;
+    display();
 
     
     
-/*}*/
+}
 
 
-//function display() {
-//    document.querySelector("#carsarea").innerHTML = "";
-//    car.forEach(t => {
-//        document.querySelector("#carsarea").innerHTML +=
-//            "<tr><td>" + t.CarsID + "</td><td>" + t.CarBrandID + "</td><td>" + t.LicensePlateNumber + "</td><td>" + t.Year + "</td><td>" + t.Type + "</td><td>" + t.PerformanceInHP + "</td><td>" + t.carBrand.CarBrandID + "</td><td>" + t.carBrand.name + "</td><td>" +
-//                `<button type="button" onclick="remove(${t.CarsID})">Delete</button>` + "</td ></tr > ";
-//    })
+function display() {
+    document.querySelector("#carsarea").innerHTML = "";
+    car.forEach(t => {
+        document.querySelector("#carsarea").innerHTML +=
+            "<tr><td>" + t.carsID + "</td><td>" + t.carBrandID + "</td><td>" + t.licensePlateNumber + "</td><td>" + t.year + "</td><td>" + t.type + "</td><td>" + t.performanceInHP + "</td><td>" + t.carBrand.carBrandID + "</td><td>" + t.carBrand.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.carsID})">Delete</button>` + "</td ></tr > ";
+    })
 
-//}
+}
 
-//function remove(id) {
-//    fetch('http://localhost:50437/cars/' + id, {
-//        method: 'DELETE',
-//        headers: {
-//            'Content-Type': 'application/json',
-//        },
-//        body: null
-//    })
-//        .then(response => response)
-//        .then(data => {
+function remove(id) {
+    fetch('http://localhost:50437/cars/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
 
-//            console.log('Success:', data);
-//            getData();
-//        })
-//        .catch((error) => {
-//            console.error('Error:', error);
-//        });
-//}
-
-
-//function create() {
-//    let names = document.querySelector("#carbrandname").value;
-//    fetch('http://localhost:50437/carbrand', {
-//        method: 'POST',
-//        headers: {
-//            'Content-Type': 'application/json',
-//        },
-//        body: JSON.stringify(
-//            {
-//                name: names
-//            }),
-//    })
-//        .then(response => response)
-//        .then(data => {
-
-//            console.log('Success:', data);
-//            getData();
-//        })
-//        .catch((error) => {
-//            console.error('Error:', error);
-//        });
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
 
 
-//}
+function create() {
+    let names = document.querySelector("#carname").value;
+    let carbrandid = document.querySelector("#carbrandid").value;
+    let license = document.querySelector("#licensePlateNumber").value;
+    let years = document.querySelector("#year").value;
+    let types = document.querySelector("#type").value;
+    let hp = document.querySelector("#performanceInHP").value;
+    
+    fetch('http://localhost:50437/cars', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                
+                carBrandID: carbrandid,
+                licensePlateNumber: license,
+                year: years,
+                type: types,
+                performanceInHP: hp,
+                
+
+
+
+
+
+                
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+
+}
--- VO1BAB_HFT_202231.JSClient/wwwroot/script.js
+++ VO1BAB_HFT_202231.JSClient/wwwroot/script.js
@@ -1,5 +1,5 @@
 ﻿let carbrand = [];
-let car = [];
+/*let car = [];*/
 
 
 getData();
@@ -17,39 +17,39 @@ getData();
 //}
 
 async function getData() {
-    //const resp1 = await fetch("http://localhost:50437/Carbrand");
-    //const data1 = await resp1.json();
+    const resp1 = await fetch("http://localhost:50437/Carbrand");
+    const data1 = await resp1.json();
 
 
-    const resp2 = await fetch("http://localhost:50437/Cars");
-    const data2 = await resp2.json();
-    console.log(data2);
-    /*carbrand = data1;*/
-    /*car = data2;*/
-   
-   /* display();*/
-   /* display2();*/
-}
-
-//function display() {
-//    document.querySelector("#resultarea").innerHTML = "";
-//    carbrand.forEach(t => {
-//        document.querySelector("#resultarea").innerHTML  += 
-//            "<tr><td>" + t.carBrandID + "</td><td>" + t.name + "</td><td>" +
-//            `<button type="button" onclick="remove(${t.carBrandID})">Delete</button>` + "</td ></tr > ";
-//    })
+    //const resp2 = await fetch("http://localhost:50437/Cars");
+    //const data2 = await resp2.json();
     
-//}
-
-function display2() {
-    document.querySelector("#carsarea").innerHTML = "";
-    car.forEach(t => {
-        document.querySelector("#carsarea").innerHTML +=
-            "<tr><td>" + t.CarsID + "</td><td>" + t.CarBrandID + "</td><td>" + t.LicensePlateNumber + "</td><td>" + t.Year + "</td><td>" + t.Type + "</td><td>" + t.PerformanceInHP + "</td><td>" + t.carBrand.CarBrandID + "</td><td>" + t.carBrand.name + "</td><td>" +
-            `<button type="button" onclick="remove(${t.CarsID})">Delete</button>` + "</td ></tr > ";
-    })
-
+    carbrand = data1;
+   /* car = data2;*/
+   
+    display();
+    display2();
 }
+
+function display() {
+    document.querySelector("#resultarea").innerHTML = "";
+    carbrand.forEach(t => {
+        document.querySelector("#resultarea").innerHTML  += 
+            "<tr><td>" + t.carBrandID + "</td><td>" + t.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.carBrandID})">Delete</button>` + "</td ></tr > ";
+    })
+    
+}
+
+//function display2() {
+//    document.querySelector("#carsarea").innerHTML = "";
+//    car.forEach(t => {
+//        document.querySelector("#carsarea").innerHTML +=
+//            "<tr><td>" + t.carsID + "</td><td>" + t.carBrandID + "</td><td>" + t.licensePlateNumber + "</td><td>" + t.year + "</td><td>" + t.type + "</td><td>" + t.performanceInHP + "</td><td>" + t.carBrand.carBrandID + "</td><td>" + t.carBrand.name + "</td><td>" +
+//            `<button type="button" onclick="remove(${t.CarsID})">Delete</button>` + "</td ></tr > ";
+//    })
+
+//}
 
 
 
