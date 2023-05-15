f5105b4f5fb880cfdd6dec213342adb25622d6e6
katonabalint <katonabalint@stud.uni-obuda.hu>
2023-05-15 22:39:51 +0200

    create rents entity create delete and get method

---
+++ VO1BAB_HFT_202231.JSClient/wwwroot/rentsapp.js
@@ -0,0 +1,79 @@
+﻿let rents = [];
+
+getData();
+
+async function getData() {
+    let resp = await fetch("http://localhost:50437/rents");
+    let data = await resp.json();
+    rents = data;
+    display();
+
+
+
+}
+
+function display() {
+    document.querySelector("#rentsarea").innerHTML = "";
+    rents.forEach(t => {
+        document.querySelector("#rentsarea").innerHTML +=
+            "<tr><td>" + t.rentId + "</td><td>" + t.rentTime + "</td><td>" + t.ownerName + "</td><td>" + t.carsID + "</td><td>" +
+            `<button type="button" onclick="remove(${t.rentId})">Delete</button>` + "</td ></tr > ";
+    })
+
+}
+
+function remove(id) {
+    fetch('http://localhost:50437/rents/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+
+function create() {
+    let renttime = document.querySelector("#renttime").value;
+    let ownername = document.querySelector("#ownername").value;
+    let carsid = document.querySelector("#carsid").value;
+
+
+
+    fetch('http://localhost:50437/rents', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                rentTime: renttime,
+                ownerName: ownername,
+                carsID: carsid
+
+
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+
+}
+
+
