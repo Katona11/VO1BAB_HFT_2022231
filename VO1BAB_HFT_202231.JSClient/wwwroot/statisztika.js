e942954b6a95663a0075c75e52d83e803516ab0a
katonabalint <katonabalint@stud.uni-obuda.hu>
2023-05-16 22:26:08 +0200

    create therentscarbrand method

---
+++ VO1BAB_HFT_202231.JSClient/wwwroot/statisztika.js
@@ -0,0 +1,42 @@
+﻿let TheRentsCarBrand = [];
+let BrandperRentsCountsMethod = [];
+
+getData();
+
+async function getData() {
+    let url1 = "http://localhost:50437/crudmethod/therentscarbrand";
+    let response1 = await fetch(url1);
+    let data1 = await response1.json();
+
+    
+    TheRentsCarBrand = data1;
+
+
+    let url2 = "http://localhost:50437/CrudMethod/BrandperRentsCountsMethod";
+    let response2 = await fetch(url2);
+    let data2 = await response2.json();
+
+    BrandperRentsCountsMethod = data2;
+    display1();
+    display2();
+
+
+}
+
+
+function display1() {
+    document.querySelector("#theRentsCarBrand").innerHTML += "<ul>";
+    TheRentsCarBrand.forEach(t =>
+    {
+        document.querySelector("#theRentsCarBrand").innerHTML += "<li>" + t + "</li>"
+    }) 
+    document.querySelector("#theRentsCarBrand").innerHTML += "</ul>";
+}
+
+function display2() {
+    document.querySelector("#BrandperRentsCountsMethod").innerHTML += "<ul>";
+    BrandperRentsCountsMethod.forEach(t => {
+        document.querySelector("#BrandperRentsCountsMethod").innerHTML += "<li>" + t.brand + "(" + t.count + ")" + "</li>";
+    })
+    document.querySelector("#BrandperRentsCountsMethod").innerHTML += "</ul>";
+}
